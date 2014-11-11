63aceb15f7f5741e228091fdce0de056fd99ba05
calife <califerno@gmail.com>
2014-11-11 12:51:24 +0100

    Prima versione con menu select

--- db/crea-script-archibus-select.sh
+++ db/crea-script-archibus-select.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# Friday, 07. November 2014
+# Tuesday, 11. November 2014
 #
 # Script di creazione degli script oracle, a partire da un TEMPLATE comune.
 #
 # Utilizza il builtin select per la presentazione dei menu
-# Es. PS3="Select a word> "; select word in ${array[@]} "exit|quit|bye" ; do echo $word ; echo "Word: $word - Choice: $REPLY" ; done
 #
 
 CWD=$(dirname $(realpath $0))
 NEW_INSTANCE_NAME="";
 NEW_AFM_PASSWORD="";
 
-# Verifica ed esegue il caricamento delle funzioni di help e della libreria zenity, per creare gli script
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+
+# Verifica ed esegue il caricamento delle funzioni di help per creare gli script
 checkRequired() {
 
 	type realpath > /dev/null || { echo "[$0] Il pacchetto realpath non è installato"; exit 1; } # check realpath
@@ -24,37 +26,73 @@ checkRequired() {
 	fi;
 }
 
-
 Usage() {
 	echo " "
-	echo " Script 0.1 Thursday, 16. October 2014"
+	echo " Script 0.1 Tuesday, 11. November 2014"
 	echo " Utilizzo: "
 	echo " "` basename $0` " new_instance_name "
 	echo " "
 }
 
-AskConfirm() {
-	echo -n ${1};
-	while true ; do	
-		read CONFIRM		
-		case $CONFIRM in
-			s|y|Y|S|YES|yes|Yes|Si|SI)
-				return 1;
-		 		break
-		 		;;			
-			n|N|no|NO|No)
-				echo -n "Operazione annullata";
-				return 0;
-		 		break
-				;;
-			*) echo -n "Operazione non valida, digitare y o n :";;
-		esac;
+showmenu() {
+
+    PS3="Scegli un' azione _$ ";
+
+
+	local array=("Script_di_base" "Cambio_password" "Modifica_tablespace_AFM_P1" "Modifica_tablespace_AFMDOCMGMT_BLOB");
+
+	echo -en "\n### Wizard di creazione script per istanza $NEW_INSTANCE_NAME ###\n"
+
+    select command in "${array[@]}" "Quit" ; do
+
+		# sleep 1;
+
+		case $REPLY in
+			1)
+				if [ $PREVIOUS_CHOICE -eq 0 -o $PREVIOUS_CHOICE -eq 1 ] ; then
+					PREVIOUS_CHOICE=1;
+					genera-script-base  "$NEW_INSTANCE_NAME"
+				else
+					echo  -e "${RED}Invalid Option${STD}"
+				fi;
+				break;;
+			2)
+				if [ $PREVIOUS_CHOICE -eq 1 -o $PREVIOUS_CHOICE -eq 2 ] ; then
+					PREVIOUS_CHOICE=2;
+					genera-script-cambio-password  "$NEW_INSTANCE_NAME"
+				else
+					echo  -e "${RED}Invalid Option, choose 1 required${STD}"
+				fi;
+				break;;
+			3)
+				if [ $PREVIOUS_CHOICE -eq 2 -o $PREVIOUS_CHOICE -eq 3 ] ; then
+					PREVIOUS_CHOICE=3;
+					genera-script-tablespace-AFM_P1 "$NEW_INSTANCE_NAME"
+				else
+					echo  -e "${RED}Invalid Option, choose 2 required${STD}"
+				fi;
+				break;;
+			4)
+				if [ $PREVIOUS_CHOICE -eq 3 -o $PREVIOUS_CHOICE -eq 4 ] ; then
+					PREVIOUS_CHOICE=4;
+					genera-script-tablespace-AFM_BLOB "$NEW_INSTANCE_NAME"
+				else
+					echo  -e "${RED}Invalid Option, choose 3 required${STD}"
+				fi;
+				break;;
+			5|quit)
+				echo "Quit";
+				exit;;
+			*) echo -e "${RED}Opzione non valida${STD}";;
+		esac
+
 	done;
+
 }
 
-############################################ Main ##############################################
+############################################ Main Loop ############################################
 
-checkRequired
+declare -i PREVIOUS_CHOICE=0;
 
 if [ $# -eq 1 ]; then
 	case "$1" in
@@ -62,32 +100,25 @@ if [ $# -eq 1 ]; then
 		--help|-h) Usage; exit 0;;
 		*  )
 
+			echo " Start... "
+
+			checkRequired;
+
 			NEW_INSTANCE_NAME=$1;
 
-			AskConfirm "Vuoi creare gli script  per la nuova istanza "$NEW_INSTANCE_NAME" ?[y/n] ";
+			while [ ! "$choice" == "tblspace_doc" ] ; do
+				showmenu;
+			done;
 
-			if [ $? != 0 ]; then
+			echo " ...Script creati in /tmp/$NEW_INSTANCE_NAME"
 
-				echo " Start... "
+			finale && exit 0;
 
-				genera-script-base  "$NEW_INSTANCE_NAME"
-
-				genera-script-cambio-password  "$NEW_INSTANCE_NAME"
-
-				genera-script-tablespace-AFM_P1 "$NEW_INSTANCE_NAME"
-
-				genera-script-tablespace-AFM_BLOB "$NEW_INSTANCE_NAME"
-
-				echo " ...Script creati in /tmp/$NEW_INSTANCE_NAME"
-			fi;
-
-			exit 0;
-
-			;;
 	esac;
 else
 	echo "Il numero di parametri forniti non e' corretto"
 	exit 1;
 fi;
 
-#####################################################################################################
+
+###################################################################################################
