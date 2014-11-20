49e72040486dcd83e7920b191c29d001027bba8f
calife <califerno@gmail.com>
2014-11-20 15:06:50 +0100

    svn_list_repositorie e create implementati

--- svn/svn.sh
+++ svn/svn.sh
@@ -3,52 +3,99 @@
 # Tuesday, 18. November 2014
 #
 
-SVN_URL="svn://svn01.efm.srl"
+# SVN_URL="svn://svn01.efm.srl"
+SVN_URL="svn://localhost"
 SVN_ROOT="/var/svn"
 NOME_REPOS=""
 
 RED='\033[0;41;30m'
-STD='\033[0;0;39m'
+RESET_COLOR='\033[0;0;39m'
+LRED=$'\033[38;5;9m'
+RESET_PS3_COLOR=$'\033[m'
+UNDERLINE=$'\033[4m'
+RESET_UNDERLINE=$'\033[24m'
+
+CODE_SUCCESS=0;
+CODE_ERROR=1;
+
+# Elenca i repository
+svn_list_repositories() {
+
+	unset MESSAGGIO
+
+	MESSAGGIO=$(printf  " | %-80s | \n\n\n " "Elenco dei repository in data $(date --iso)  ")
+	MESSAGGIO="$MESSAGGIO"$(for i in "$SVN_ROOT"/*; do  printf " | %-40s | %-40s | \n " $(basename $i) $SVN_URL/$(basename $i) ; done;)
+
+	return $CODE_SUCCESS
+
+}
 
 # Crea un bare repository
 svn_create_repository() {
 
-return;
+	unset MESSAGGIO
 
-	if [ -z "$NOME_REPOS" ] ; then
-		read -p "Nome del repository da creare > " NOME_REPOS
-	fi;
+	read -p "Repository da creare > " NOME_REPOS
 	
 	if [  -z "$NOME_REPOS" ] ; then
+
 		svn_create_repository
-	else 
 
-		test -d "$SVN_ROOT"/"$NOME_REPOS" && ( echo "Repository $NOME_REPOS gia esistente...exit" ; exit 0; )
+	else
 
-		sudo svnadmin create "$NOME_REPOS"
+        # command test returns 1 on error, 0 on ok
+		if [ -d "$SVN_ROOT"/"$NOME_REPOS" ] ; then 
+			MESSAGGIO="Repository $NOME_REPOS già esistente..."
+			return $CODE_ERROR
+		fi;
+
+        # command svnadmin returns 1 on error, 0 on ok
+		sudo svnadmin create "$SVN_ROOT/$NOME_REPOS"  >/dev/null 2>&1
+		if [ "$?" -eq 0  ] ; then
+			MESSAGGIO="Repository $NOME_REPOS creato"
+			return $CODE_SUCCESS
+		else
+			MESSAGGIO="Errore in svnadmin create..."
+			return $CODE_ERROR
+		fi;
 
 	fi;
 
 }
 
 # Permette l' amministrazione di base degli utenti
+# Aggiunta/Rimozione/Modifica password
 svn_admin_user() {
 
-return;
+	unset MESSAGGIO
 
-	test -z "$NOME_REPOS" && (echo "Nome del repository non specificato...exit"; exit 0)
-	test -d "$SVN_ROOT"/"$NOME_REPOS" || ( echo "Repository $NOME_REPOS inesistente ...exit" ; exit 0; )
+	read -p "Repository da amministrare > " NOME_REPOS
 
-    # Replace in file svnserve.conf , append user to passwd (files)
+	if [  -z "$NOME_REPOS" ] ; then
+		svn_admin_user;
+	fi;
 
+	if [ ! -d "$SVN_ROOT"/"$NOME_REPOS" ] ; then
+		MESSAGGIO="Il repository $NOME_REPOS non esiste..."
+		return $CODE_ERROR
+	else
+
+		echo " Start... " && clear && showadminmenu
+		
+        # TODO Replace in file svnserve.conf , append user to passwd (files)
+		return $CODE_SUCCESS
+
+	fi; 
+
+    
 }
 
-# Importa una specifica versione di archibus all' interno del repository
+# Importa una specifica versione di Archibus WebCentral all' interno del trunk del repository
 svn_import_archibus() {
 
-return;
+	return;
 
-	test -z "$NOME_REPOS" && (echo "Nome del repository non specificato...exit"; exit 0)
+	test -z "$NOME_REPOS" && (echo "Repository non specificato...exit"; exit 0)
 	test -d "$SVN_ROOT"/"$NOME_REPOS" || ( echo "Repository $NOME_REPOS inesistente ...exit" ; exit 0; )
 
 	svn import -m "Initial import" archibus "$SVN_URL"/"$NOME_REPOS"/trunk/archibus
@@ -60,54 +107,105 @@ return;
 
 }
 
+# Elenca le versioni di Archibus WebCentral disponibili
 svn_show_repository() {
 	return;
 }
 
-# Scarica la versione di archibus in formato war fornita come parametro
+# Scarica la versione di Archibus WebCentral in formato war
 download_archibus() {
- ::::
+	::::
 }
 
-showmenu() {
+# Mostra il menu di amministrazione degli utenti
+showadminmenu() {
+
+	echo " Replace in file svnserve.conf , append user to passwd (files)"
+
+	# comandi_amministrazione=(
+	# 	"Creazione utente      "
+	# 	"Elimina utente        "
+	# 	"Elenca utenti         "
+	# 	"Cambia password       "
+    # );
+
+	# while true; do
+
+	# 	echo -en "\n### Menu per la gestione degli utenti  ###\n"
+
+	# 	PS3="${LRED}$MESSAGGIO${RESET_PS3_COLOR} Scegli un' azione _$ "
+
+	# 	select command in "${comandi_amministrazione[@]}" "Quit" ; do
+
+	# 		case $REPLY in
+	# 			5|quit)
+	# 				return $CODE_SUCCESS
+	# 				break;;
+	# 			*) 
+	# 				unset MESSAGGIO
+	# 				MESSAGGIO="Opzione $REPLY non valida"
+    #                 break;;
+	# 		esac;
+
+	# 	done;
+
+	# done;
+
+}
+
+
+show_main_menu() {
 
 	comandi=(
-		"Creazione repository                     "
+		"Elenca repository                        "
+		"Crea repository                          "
 		"Amministrazione utenti                   "
 		"Caricamento di Archibus Webcentral       "
 		"Visualizza informazioni repository       "
+		"Quit                                     "
 	);
-
-    PS3="Scegli un' azione _$ ";
-	COMPLETED_STR=" [COMPLETED] "
 
 	while true; do
 
-		echo -en "\n### Menu di gestione repository Subversion ${RED}$NOME_REPOS${STD} ###\n"
+		echo -en "\n### Menu per la gestione dei repository Subversion ###\n"
 
-		select command in "${comandi[@]}" "Quit" ; do
+		PS3="${LRED}$MESSAGGIO${RESET_PS3_COLOR}"$'\n'"Scegli un' azione _$ "
 
-			case $REPLY in
+		select command in "${comandi[@]}" ; do
 
-				1|2|3|4)
+			case "$command" in
 
-					comandi[$(($REPLY-1))]=${comandi[$(($REPLY-1))]/"$COMPLETED_STR"/}"$COMPLETED_STR"
-
-					if [ $REPLY -eq 1 ] ; then svn_create_repository ; fi;
-
-					if [ $REPLY -eq 2 ] ; then svn_admin_user  "$NOME_REPOS"; fi;
-
-					if [ $REPLY -eq 3 ] ; then svn_import_archibus "$NOME_REPOS"; fi;
-
-					if [ $REPLY -eq 4 ] ; then svn_show_repository "$NOME_REPOS"; fi;
-
-					break;;
-
-				5|quit)
+				"Elenca repository"*)
+					svn_list_repositories
+					break
+					;;
+				"Crea repository"*)
+					svn_create_repository
+					break
+					;;
+				"Amministrazione utenti"*)
+					svn_admin_user
+					break
+					;;
+				"Caricamento di Archibus Webcentral"*)
+					svn_import_archibus
+					break
+					;;
+				"Visualizza informazioni repository"*)
+					svn_show_repository
+					break
+					;;
+				"Quit"*)
 					echo "Quit";
-					exit;;
-				*) echo -e "${RED}Opzione non valida${STD}";;
+					exit
+					;;
+				*) 
+					unset MESSAGGIO			
+					clear	
+                    break;;
 			esac
+
+
 
 		done;
 
@@ -125,6 +223,6 @@ if [ $# -eq 1 ]; then
 	esac;
 fi;
 
-echo " Start... " && clear && showmenu && echo " ...Repository creato, URL: $SVN_URL/$NOME_REPOS"
+echo " Start... " && clear && show_main_menu
 
 ############################################
