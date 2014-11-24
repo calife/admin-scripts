4dd0c85718a3d19cf1e04e2551a3b901531bfe12
calife <califerno@gmail.com>
2014-11-24 17:51:06 +0100

    Versione riveduta e aggiornata

--- svn/efm_svn_admin.sh
+++ svn/efm_svn_admin.sh
@@ -3,7 +3,6 @@
 # Friday, 21. November 2014
 #
 
-# SVN_URL="svn://svn01.efm.srl"
 SVN_URL="svn://localhost"
 SVN_ROOT="/var/svn"
 NOME_REPOS=""
@@ -42,8 +41,8 @@ svn_list_repositories() {
 	MESSAGGIO="OUTPUT:"
 	MESSAGGIO+=$(printf  "  %-80s    \n " "   ")
 	MESSAGGIO+=$(printf  "  %-80s    \n\n\n " "Elenco dei repository in data $(date --iso)  ")
-	MESSAGGIO+=$(for i in "$SVN_ROOT"/*; do  printf " %-40s %-40s \n " $(basename $i) $SVN_URL/$(basename $i) ; done;)
-
+	MESSAGGIO+=$(repos=(`ls -1 "$SVN_ROOT"/`) ; printf "\t%-40s%-40s%-40s\n" "${repos[@]}")
+	
 	return $CODE_SUCCESS
 
 }
@@ -65,16 +64,18 @@ svn_create_repository() {
 			return $CODE_ERROR
 		fi;
 
-		sudo svnadmin create "$SVN_ROOT/$NOME_REPOS"  >/dev/null 2>&1
+		sudo -p "Inserisci la password per sudo > " svnadmin create "$SVN_ROOT/$NOME_REPOS"
 		if [ "$?" -eq 0  ] ; then
+
+			sudo  -p "Inserisci la password per sudo > " sh -c " sed -i \"s/# password-db = passwd/password-db = passwd/g\" \"$SVN_ROOT/$NOME_REPOS/conf/svnserve.conf\"  "
+
 			MESSAGGIO="SUCCESS:Repository $NOME_REPOS creato"
 			return $CODE_SUCCESS
-		else
+		eles
 			MESSAGGIO="ERROR:Errore in svnadmin create..."
 			return $CODE_ERROR
 		fi;
 
-		sudo sh -c " sed -i \"s/# password-db = passwd/password-db = passwd/g\" \"$SVN_ROOT/$NOME_REPOS/conf/svnserve.conf\"  "
 
 	fi;
 
@@ -97,7 +98,14 @@ svn_show_repository() {
 		MESSAGGIO="OUTPUT:"
 		MESSAGGIO+=$(printf  "  %-80s    \n " "   ")
 		MESSAGGIO+=$(printf  " %-80s \n\n " "Informazioni repository $SVN_URL/$NOME_REPOS in data $(date --iso)  ")
-		MESSAGGIO+=$(cat "$SVN_ROOT"/"$NOME_REPOS"/conf/passwd |grep -v "^#"|grep -v "^$")
+
+		MESSAGGIO+=$(printf  "  %-80s    \n " "[Layout]")
+		MESSAGGIO+=$(for i in "`svn ls $SVN_URL/$NOME_REPOS`"; do printf "%-50s\n" "$i" ; done;)
+		MESSAGGIO+=$(printf  "  %-80s    \n " "")
+
+		MESSAGGIO+=$(printf  "  %-80s    \n " "[Users]")
+		str=`cat "$SVN_ROOT"/"$NOME_REPOS"/conf/passwd |grep -v "^#"|grep -v "^$"|grep -v "\[users\]"|sed -n 's/^\(.*\)=.*$/\1/p' `
+        MESSAGGIO+=$(printf  "  %-5s " `echo "$str"`)
 
 		return $CODE_SUCCESS
 
@@ -136,7 +144,7 @@ svn_add_user() {
 
 	row=$(echo "$username = $password")
 
-    sudo sh -c " echo $row >> $SVN_ROOT"/"$NOME_REPOS/conf/passwd "
+    sudo  -p "Inserisci la password per sudo > " sh -c " echo $row >> $SVN_ROOT"/"$NOME_REPOS/conf/passwd "
 
 	if [ "$?" -eq 0  ] ; then
 		MESSAGGIO="SUCCESS:Utenza $username creata"
@@ -164,7 +172,7 @@ svn_rm_user() {
 		return $CODE_ERROR
 	fi;
 
-    sudo sh -c " sed -i \"/^$username[ ]*=.*/d\" \"$SVN_ROOT/$NOME_REPOS/conf/passwd\" "
+    sudo  -p "Inserisci la password per sudo > " sh -c " sed -i \"/^$username[ ]*=.*/d\" \"$SVN_ROOT/$NOME_REPOS/conf/passwd\" "
 
 	if [ "$?" -eq 0  ] ; then
 		MESSAGGIO="SUCCESS:Utenza $username rimossa"
@@ -199,7 +207,7 @@ svn_change_pwd() {
 
 	row=$(echo "$username = $password")
 
-    sudo sh -c " sed -i \"s/^$username[ ]*=.*/$row/g\" \"$SVN_ROOT/$NOME_REPOS/conf/passwd\" "
+    sudo  -p "Inserisci la password per sudo > " sh -c " sed -i \"s/^$username[ ]*=.*/$row/g\" \"$SVN_ROOT/$NOME_REPOS/conf/passwd\" "
 
 	if [ "$?" -eq 0  ] ; then
 		MESSAGGIO="SUCCESS:Password cambiata"
@@ -224,11 +232,12 @@ svn_import_archibus() {
 	test -z "$NOME_REPOS" && (echo "Repository non specificato...exit"; exit 0)
 	test -d "$SVN_ROOT"/"$NOME_REPOS" || ( echo "Repository $NOME_REPOS inesistente ...exit" ; exit 0; )
 
-	svn import -m "Initial import" archibus "$SVN_URL"/"$NOME_REPOS"/trunk/archibus
+	# svn import -m "Initial import" archibus "$SVN_URL"/"$NOME_REPOS"/trunk/archibus
 
-	for i in tags branches; do
-		svn mkdir "$SVN_URL"/"$NOME_REPOS"/$i -m "Creating $i folder"
-		svn mkdir "$SVN_URL"/"$NOME_REPOS"/$i -m "Creating $i folder"
+	for i in trunk tags branches; do
+		svn mkdir "$SVN_URL"/"$NOME_REPOS"/$i -m "Creating $i folder"  >/dev/null 2>&1
+		svn mkdir "$SVN_URL"/"$NOME_REPOS"/$i -m "Creating $i folder"  >/dev/null 2>&1
+		svn mkdir "$SVN_URL"/"$NOME_REPOS"/$i -m "Creating $i folder"  >/dev/null 2>&1
 	done;
 
 }
@@ -276,7 +285,6 @@ show_main_menu() {
 		"Elenca repository                        "
 		"Crea repository                          "
 		"Menu Amministrazione utenti              "
-		"Caricamento di Archibus Webcentral       "
 		"Visualizza informazioni repository       "
 		"Quit                                     "
 	);
@@ -371,7 +379,7 @@ show_main_menu() {
 					echo "Quit";
 					exit;;
 				*) 
-					unset MESSAGGIO			
+					unset MESSAGGIO
 					clear	
                     break;;
 			esac
@@ -384,12 +392,6 @@ show_main_menu() {
 
 ################### Main ###################
 
-if [ $# -eq 1 ]; then
-	case "$1" in
-		--help|-h) Usage; exit 0;;
-	esac;
-fi;
-
-echo " Start... " && clear && show_main_menu
+echo " Start... " && clear && show_main_menu && echo "...leave"
 
 ############################################
